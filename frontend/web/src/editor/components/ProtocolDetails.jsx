2dc366580fca071f4a534e7b72629c7acd0eea81
Daniel Bryce <danbryce410@gmail.com>
2022-10-25 22:42:28 -0500

    added graphviz to inspector

--- frontend/web/src/editor/components/ProtocolDetails.jsx
+++ frontend/web/src/editor/components/ProtocolDetails.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Modal, Button} from 'react-bootstrap';
 import Markdown from 'marked-react';
 import DOMPurify from 'dompurify';
 // import { JSONEditor } from 'vanilla-jsoneditor';
 // import { useEffect, useRef } from "react";
 import ReactJson from 'react-json-view'
 
-
+import { Graphviz } from 'graphviz-react';
 
 export class ProtocolDetailsGroup extends React.Component {
+    state = { show: false };
+    handleClick = () => {
+        this.setState({ show: true })
+    }
+    handleClose = () => {
+        this.setState({ show: false })
+    }
 
     render() {
         let specializations = this.props.editor.state.specializations;
@@ -29,10 +36,46 @@ export class ProtocolDetailsGroup extends React.Component {
                     if (protocol) {
                         let spec = protocol.specializations.find((s) => (s && s.specialization_id === specialization.id));
                         if (spec && spec.data) {
-                            let rendered = (specialization.name === "DefaultBehaviorSpecialization") ?
-                                (<ReactJson key={specialization.specialization_id} src={JSON.parse(spec.data)} //
-                                    displayDataTypes="False" name="Steps" indentWidth="2" />) :
-                                (<ProtocolInspector key={specialization.specialization_id} specialization={spec.data} />);
+                            let rendered = null;
+                            if (specialization.name === "DefaultBehaviorSpecialization") {
+
+                                let protocol_data = JSON.parse(spec.data);
+                                let steps = protocol_data.slice(0, -1);
+                                let gviz = protocol_data.slice(-1)[0];
+                                rendered = (
+                                    <div>
+                                        <Modal size="lg" show={this.state.show} onHide={this.handleClose} >
+                <Modal.Header closeButton>
+                    Protocol Execution (Graphviz)
+                </Modal.Header>
+                                            <Modal.Body >
+                                                <div style={{ flex: "auto", overflowX: "scroll", overflowY: "scroll"}}>
+                                                <Graphviz dot={gviz} options={
+                                                    {
+                                                        // fit: true,
+                                                        height: 750,
+                                                        width: 1500,
+                                                        zoom: true
+                                                    }} />
+                                                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+            </Modal>
+                                    <Button onClick={this.handleClick}>View Execution Graph</Button>
+                                    <ReactJson
+                                        key={specialization.specialization_id}
+                                        src={steps}
+                                            displayDataTypes="False" name="Steps" indentWidth="2" />
+                                    </div>
+                                );
+                            } else {
+                                rendered = (
+                                    <ProtocolInspector
+                                        key={specialization.specialization_id}
+                                        specialization={spec.data} />
+                                );
+                            }
                             return rendered;
                         }
                     }
